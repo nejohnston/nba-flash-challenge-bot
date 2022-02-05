c2b56b3ceaca65dbc73e1d7067f3a7b3b093255d
Nicholas Johnston <nicholas.johnston94@gmail.com>
2022-02-04 23:48:17 -0800

    initial setup of discord bot

---
+++ bot.py
@@ -0,0 +1,47 @@
+"""
+Version: 0.1.0
+Author: Nicholas Johnston
+Created: February 4th, 2022
+Last Updated: February 4th, 2022
+"""
+import os
+import random
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
+
+
+client.run(TOKEN)
+
+
+def main():
+    print("hello world")
+
+
+if __name__ == '__main__':
+    main()
